ead144ff0b1961da3747c8edb55d09be05c92ac8
KaoHu <kaohulogist@gmail.com>
2022-07-23 04:42:32 -0500

    first commit

---
+++ sp500-app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import yfinance as yf
+
+st.title('S&P 500 App')
+
+st.markdown("""
+This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+""")
+
+st.sidebar.header('User Input Features')
+# Web Scraping of S&P 500 Stock Data
+@st.cache
+
+def get_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    return df
+
+df = get_data()
+sector = df.groupby('GICS Sector')
